06deb2fcb365606ef53c92150f795bbcd444ed90
GF3000 <94068411+GF3000@users.noreply.github.com>
2024-05-17 12:09:14 +0200

    Vista TODO

---
+++ src/main/java/es/upm/pproject/sokoban/Vista.java
@@ -0,0 +1,11 @@
+package main.java.es.upm.pproject.sokoban;
+
+public class Vista {
+    public Vista() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void pintar(Partida partida) {
+        // TODO Auto-generated method stub
+    }
+}
