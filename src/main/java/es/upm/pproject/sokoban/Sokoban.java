53240212d66663f7c8a7275eb12fcd42ac66d1d7
viiiccv <95257527+viiiccv@users.noreply.github.com>
2024-05-20 17:11:50 +0200

    Sokoban.java main insertado

--- src/main/java/es/upm/pproject/sokoban/Sokoban.java
+++ src/main/java/es/upm/pproject/sokoban/Sokoban.java
@@ -1,7 +1,24 @@
 package es.upm.pproject.sokoban;
 
+import javax.swing.SwingUtilities;
+
 public class Sokoban {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Nivel n = new Nivel(1);
+            Partida p = new Partida(n,0,1);
+            Vista v = new Vista();
+            Controlador c = new Controlador(v, p);
+            c.getPartida().setControlador(c);
+            c.getVista().setControlador(c);
+            
+            SwingUtilities.invokeLater(() -> {
+                c.getVista().pintar(p);
+                c.getVista().setVisible(true);
+            });        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- src/main/java/es/upm/pproject/sokoban/Vista.java
+++ src/main/java/es/upm/pproject/sokoban/Vista.java
@@ -159,24 +159,4 @@ public class Vista extends JFrame {
         return images;
     }
   
-    public static void main(String[] args) {
-       
-        try {
-            Nivel n = new Nivel(1);
-            Partida p = new Partida(n,0,1);
-            Vista v = new Vista();
-            Controlador c = new Controlador(v, p);
-            c.getPartida().setControlador(c);
-            c.getVista().setControlador(c);
-            
-            SwingUtilities.invokeLater(() -> {
-                // Vista vista = new Vista(c);
-                c.getVista().pintar(p);
-                c.getVista().setVisible(true);
-            });
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
