0a05649239be3412f91bd997074de084b66029f6
GF3000 <94068411+GF3000@users.noreply.github.com>
2024-06-07 18:06:46 +0200

    Metodos guardar y cargar partida controlador

--- src/main/java/es/upm/pproject/sokoban/Controlador.java
+++ src/main/java/es/upm/pproject/sokoban/Controlador.java
@@ -1,21 +1,20 @@
 package es.upm.pproject.sokoban;
 
-/*
- * Controlador	
-	+ Partida
-	+ Vista
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
-	Contrutor: Controlador(vista, partida)
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	- ejecutarTecla(char)//decide qué hacer cuando se pulsa una tecla, llama a partida.mover
-	- cargarPartida(path) // Partida = new Partida
-	- guardarPartida(path)
-	- mostrar() // llama a Vista.pintar(Partida)
- */
 public class Controlador implements ControladorInterface {
 
     private static PartidaInterface partida;
     private Vista vista;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controlador.class);
+
 
     /**
      * Constructor de la clase Controlador
@@ -25,6 +24,7 @@ public class Controlador implements ControladorInterface {
     public Controlador(Vista vista, PartidaInterface partida) {
         this.vista = vista;
         this.partida = partida;
+
     }
 
 
@@ -47,12 +47,39 @@ public class Controlador implements ControladorInterface {
     }
 
 
+    @Override
     public void cargarPartida(String path) {
-        //TODO: Cargar partida JSON
+        try {
+            File file = new File(path);
+            JAXBContext jaxbContext = JAXBContext.newInstance(Partida.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Partida partidaCargada = (Partida) jaxbUnmarshaller.unmarshal(file);
+            
+            // Settear el controlador actual en la partida cargada si es necesario
+            partidaCargada.setControlador(this);
+            
+            // Settear la partida cargada en el controlador
+            partida = partidaCargada;
+
+            // Opcional: actualizar la vista
+            actualizarTablero();
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void guardarPartida(String path) {
-        //TODO: Convertir partida a JSON
+     public void guardarPartida(String path) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Partida.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(partida  , new File(path));
+            LOGGER.debug("Partida guardada en {}", path);
+        } catch (JAXBException e) {
+            LOGGER.error("Error al guardar la partida en {}: {}", path, e.getMessage());
+        }
     }
 
     public void actualizarTablero() {
